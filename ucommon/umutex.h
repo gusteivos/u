d61232693018e507dfae5033ad1d865bea2c4332
Gustavo Ramos Carvalho <gc5142387@gmail.com>
2024-11-06 21:48:41 -0300

    add mutex modes and update function signatures

--- ucommon/umutex.h
+++ ucommon/umutex.h
@@ -3,20 +3,31 @@
 #define UMUTEX_H
 
 #include <stddef.h>
+#include <errno.h>
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-typedef void * (* umutex_create_t  )(void  );
+typedef enum umutex_mode_e
+{
 
-typedef int    (* umutex_lock_t    )(void *);
+    UMUTEX_MODE_NORMAL,
+    UMUTEX_MODE_REAWIR,
+    UMUTEX_MODE_REAWIR_SHARED,
+    UMUTEX_MODE_REAWIR_EXCLUSIVE
 
-typedef int    (* umutex_try_lock_t)(void *);
+} umutex_mode_t;
 
-typedef int    (* umutex_unlock_t  )(void *);
+typedef void *(* umutex_create_t)(umutex_mode_t);
 
-typedef void   (* umutex_destroy_t )(void *);
+typedef int (* umutex_lock_t)(umutex_mode_t, void *);
+
+typedef int (* umutex_try_lock_t)(umutex_mode_t, void *);
+
+typedef int (* umutex_unlock_t)(umutex_mode_t, void *);
+
+typedef void (* umutex_destroy_t)(umutex_mode_t, void *);
 
 #ifdef __cplusplus
 }
